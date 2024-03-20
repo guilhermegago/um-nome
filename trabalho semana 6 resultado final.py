40b4362f2dbe4cbf47c005050ab3c9e0ac875781
guilhermegago <160735800+guilhermegago@users.noreply.github.com>
2024-03-20 09:37:33 -0300

    Add files via upload

---
+++ trabalho semana 6 resultado final.py
@@ -0,0 +1,33 @@
+Python 3.10.0 (tags/v3.10.0:b494f59, Oct  4 2021, 19:00:18) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:/Users/202102307147/Desktop/Pastinha dos exercícios/exercicio meu nome.py
+Guilherme
+nome
+'Guilherme'
+numero
+22
+booleano
+True
+numero + 2
+24
+
+= RESTART: C:/Users/202102307147/Desktop/Pastinha dos exercícios/exercicio meu nome 2 v.py
+Guilherme
+Média aritmética dos números: 25.0
+Quadrado do terceiro número: 900
+Dobro do segundo número: 40
+Quantidade de letras na palavra: 11
+Quantidade de espaços em branco na frase: 6
+O primeiro número é maior que o segundo: False
+O maior número é: 40
+
+======== RESTART: C:/Users/202102307147/Desktop/exercicio meu nome 3.py ========
+Guilherme
+Média aritmética dos números: 12.5
+Quadrado do terceiro número: 169
+Dobro do segundo número: 24
+Quantidade de letras na palavra: 7
+Quantidade de espaços em branco na frase: 6
+O primeiro número é maior que o segundo: False
+O maior número é: 14
